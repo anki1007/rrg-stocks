0c2b6a082cc903d54302b1a5e72a9ff3741c669f
anki1007 <anki.agarwal23@gmail.com>
2025-12-29 00:59:36 +0530

    optuma.py

--- optuma.py
+++ optuma.py
@@ -873,8 +873,7 @@ if st.session_state.df_cache is not None:
                     table_rows += f"""
                     <tr>
                         <td style="text-align: center;">{int(row['Sl No.'])}</td>
-                        <td class="symbol-cell"><a href="{row['TV Link']}" target="_blank">{row['Symbol']}</a></td>
-                        <td class="name-cell">{row['Name']}</td>
+                        <td class="symbol-cell"><a href="{row['TV Link']}" target="_blank">{row['Name']}</a></td>
                         <td class="industry-cell">{row['Industry']}</td>
                         <td style="text-align: right;">₹{row['Price']:,.2f}</td>
                         <td style="text-align: right; color: {chg_color}; font-weight: 600;">{row['Change %']:+.2f}%</td>
@@ -1439,3 +1438,4 @@ if st.session_state.df_cache is not None:
 
 else:
     st.info("⬅️ Select indices and click **Load Data** to start analysis")
+
